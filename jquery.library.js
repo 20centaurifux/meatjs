c3e439871cc67f40f599ddf694291202bcaaec58
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2016-03-16 17:32:37 +0100

    changed argument type in jquery.library::onCompare(), sort popular images

--- jquery.library.js
+++ jquery.library.js
@@ -48,8 +48,10 @@
       if(li.get(0))
       {
         // update details of already inserted image:
-        $(li).find('p[data-field="comments"]').html('<strong>Comments:</strong> ' + image["comments_n"]);
-        $(li).find('p[data-field="score"]').html('<strong>Score:</strong> ' + image["score"]["up"] + ' up, ' + image["score"]["down"] + ' down, ' + image["score"]["fav"] + ' favs');
+        $(li).find('span[data-field="comments_n"]').html(image["comments_n"]);
+        $(li).find('span[data-field="up"]').html(image["score"]["up"]);
+        $(li).find('span[data-field="down"]').html(image["score"]["down"]);
+        $(li).find('span[data-field="fav"]').html(image["score"]["fav"]);
       }
       else
       {
@@ -59,9 +61,14 @@
         var html = '<li data-source="' + image["source"] + '" data-timestamp="' + image["created_on"]["$date"] + '">' +
                    '<a href="#"><img data-source="' + image["source"] + '"src="images/image-loader.gif" alt="">' +
                    '<h2>' + image["source"] + '</h2>' +
-                   '<p data-field="timestamp"><strong>Created on:</strong> ' + new Date(image["created_on"]["$date"]).toLocaleString() + '</p>' +
-                   '<p data-field="comments"><strong>Comments:</strong> ' + image["comments_n"] + '</p>' +
-                   '<p data-field="score"><strong>Score:</strong> ' + image["score"]["up"] + ' up, ' + image["score"]["down"] + ' down, ' + image["score"]["fav"] + ' favs</p>' +
+                   '<p><strong>Created on: </strong>' +
+                   '<span data-field="date">' + new Date(image["created_on"]["$date"]).toLocaleString() + '</span></p>' +
+                   '<p><strong>Comments: </strong>' +
+                   '<span data-field="comments_n">' + image["comments_n"] + '</span></p>' +
+                   '<p><strong>Score: </strong>' +
+                   '<span data-field="up">' + image["score"]["up"] + '</span> up,' +
+                   '<span data-field="down">' + image["score"]["down"] + '</span> down, ' +
+                   '<span data-field="fav">' + image["score"]["fav"] + '</span> favs</p>' +
                    '</a></li>';
 
         var inserted = false;
@@ -70,7 +77,17 @@
         {
           $(this).find("li").each(function()
           {
-            if(compare(image, $(this)))
+            var obj = {};
+
+            obj["source"] = $(this).data("source");
+            obj["created_on"] = {"$data": $(this).data("timestamp")}
+            obj["comments_n"] = parseInt($(li).find('span[data-field="comments_n"]').text(), 10);
+            obj["score"] = {}
+            obj["score"]["up"] = parseInt($(li).find('span[data-field="up"]').text(), 10);
+            obj["score"]["down"] = parseInt($(li).find('span[data-field="down"]').text(), 10);
+            obj["score"]["fav"] = parseInt($(li).find('span[data-field="fav"]').text(), 10);
+
+            if(compare(image, obj))
             {
               $(html).insertBefore(this);
               inserted = true;
